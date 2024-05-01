6740c1ba5d88d3a5dedb7bf0bf31632c49dc4112
moksh <moksh.sharma@edugorilla.org>
2024-05-01 15:18:48 +0530

    Models defination

---
+++ vendor_management/models/purchase_order.py
@@ -0,0 +1,20 @@
+from django.db  import models
+from django.utils import timezone
+
+from vendor_management.models import Vendor
+
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=20, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.DO_NOTHING)
+    order_date = models.DateTimeField(default=timezone.now)
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20, default='pending')
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField(default=timezone.now)
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.po_number
